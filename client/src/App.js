00130ef974981533a4b7aa572d257000a60bf18e
whitneyxlachelle <whiggins16@yahoo.com>
2019-11-03 18:50:46 -0600

    mvp met

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+import MoviesList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 import SavedList from './Movies/SavedList';
 
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -43,7 +43,7 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars && stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
